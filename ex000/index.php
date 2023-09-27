93d0becccb0fbeeaa86e66749b7bebb7890b85f1
FlavioAraujo23 <flaviolopes1027@gmail.com>
2023-09-27 17:58:12 -0300

    first commit

---
+++ ex000/index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>livrando da maldição</title>
+</head>
+<body>
+  <h1>
+    <?php 
+      echo "Olá, mundo! \u{1F61C}" ;
+    ?>
+  </h1>
+</body>
+</html>
